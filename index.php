44651b21912691bece8673044d8340e897cc58ee
Bonniezilla <bonnieevil86@gmail.com>
2022-11-03 15:53:27 -0300

    netbeans

--- index.php
+++ index.php
@@ -1,5 +1,5 @@
-    <!DOCTYPE html>
-    <html lang="pt-br">
+<!DOCTYPE html>
+<html lang="pt-br">
 
     <head>
         <meta charset="UTF-8">
@@ -36,21 +36,25 @@
 
 
             <?php
-            function exibirMenu()
-            {
-            ?>
-                <h2>Menu</h2>
-        </div>
-        <div class="menu">
-                <a class="buttons" href="index.php?op=lista_func"> Relação de Funcionários</a>
-                <a class="buttons" href="index.php?op=consult_func">Consulta de Funcionário</a>
-                <a class="buttons" href="index.php?op=cadastro_func">Cadastro de Funcionário</a>
-        </div>
-        
-        <script src="./js/default.js"></script>
-        <?php
-                }
+
+            function exibirMenu() {
                 ?>
+                <h2>Menu</h2>
+            </div>
+            <main>
+                <div class="buttons">
+                    <a href="index.php?op=lista_func"> Relação de Funcionários</a>
+                    <a href="index.php?op=consult_func">Consulta de Funcionário</a>
+                    <a href="index.php?op=cadastro_func">Cadastro de Funcionário</a>
+
+                </div>
+
+            </main>
+
+            <script src="./js/default.js"></script>
+            <?php
+        }
+        ?>
     </body>
 
-    </html>
+</html>
